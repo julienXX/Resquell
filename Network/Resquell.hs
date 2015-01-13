e49f92fa2dcc5de699913befb6f6a9d4d4eae231
Julien Blanchard <julien@sideburns.eu>
2015-01-13 14:38:50 +0100

    Enqueue a job in Redis

--- Network/Resquell.hs
+++ Network/Resquell.hs
@@ -1,21 +1,33 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Network.Resquell where
 
 import           Data.Aeson
+import qualified Data.ByteString               as B
+import qualified Data.ByteString.Lazy          as BL
+import           Database.Redis
 import           GHC.Generics
 
-data Queue = Queue { queueName :: String } deriving (Show)
+type Queue = B.ByteString
 
 data ResqueJob = ResqueJob { className :: String
-                           , args      :: String
-                           } deriving (Show, Generic)
+                           , args      :: [String]
+                           }
+               deriving (Show, Generic)
 
 instance FromJSON ResqueJob
-instance ToJSON ResqueJob
+instance ToJSON ResqueJob where
+  toJSON (ResqueJob className args) =
+    object [ "class"  .= className
+           , "args"   .= args ]
 
-enqueue :: Queue -> ResqueJob -> Bool
-enqueue (Queue {queueName = "resque:queue:default"}) job = True
+enqueue :: Queue -> ResqueJob -> IO (Either Reply Integer)
+enqueue queue job = do
+  conn <- connect defaultConnectInfo
+  runRedis conn $ do
+    sadd "queues" [queue]
+    rpush queue [toStrict (encode job)]
 
-perform :: ResqueJob -> Bool
-perform job = True
+toStrict :: BL.ByteString -> B.ByteString
+toStrict = B.concat . BL.toChunks
